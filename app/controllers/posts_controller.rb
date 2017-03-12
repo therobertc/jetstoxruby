e992b5b1d36fb20cbda90d75a1bfced4b334dc9e
robert calderon <rob.jcalderon@gmail.com>
2017-03-12 03:18:16 +0000

    Added a landing page with panels to log in and sign up.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     
     def create
         @post = Post.new(post_params)
-        @post.user_id = current_user.id
+        @post.user_id = current_user.id # assign the post to the user who created it.
         respond_to do |f|
             if (@post.save)
                 f.html { redirect_to "", notice: "Post created!" }
